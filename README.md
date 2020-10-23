288ec681dd4cb5892c7e5072bd90a7c5698d9b89
叶曦禾 <55824353+yexihe-jpg@users.noreply.github.com>
2020-10-23 11:00:15 +0800

    增加了 lab_1 的内容

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CV-lab
-SDU软件计算机视觉实验报告
+> SDU软件计算机视觉实验报告
+
+## lab1 图像加载、显示
+### lab 1-1：图像显示
+> **实验要求** 
+>
+>1.利用图像库的功能，实现从文件加载图像，并在窗口中进行显示的功能；
+>
+>2.利用常见的图像文件格式（.jpg； .png； .bmp； .gif）进行测试。
+>
+#### 实验结果：
+![](https://cdn.jsdelivr.net/gh/Collapsar-G/image/img/20201023105754.png)
+
+
+
+### lab 1-2：图像合成
+> **实验要求：**
+>
+>1.现有一张4通道透明图像a.png:
+>
+>2.从其中提取出alpha通道并显示;
+>
+>3.用alpha混合，为a.png替换一张新的背景（bg.png）。
+
+#### 实验结果
+![](https://cdn.jsdelivr.net/gh/Collapsar-G/image/img/20201023105512.png)
---
+++ lab1/lab1_2.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import pyglet
+""""""
+""""""
+"lab 1-2 图像合成"
+""""""
+""""""
+img_path_2 = r'./image/lab2/'
+
+#读入图像
+a_png = cv2.imread(img_path_2+"a.png",-1)
+cv2.imshow("a.png",a_png)
+cv2.waitKey(0)
+
+#将图像通道分离
+b,g,r,a = cv2.split(a_png)
+
+#得到PNG图像的alpha通道，即alpha掩模
+alpha = cv2.merge((a,a,a))
+# 保存alpha图像
+cv2.imwrite(img_path_2+"alpha.jpg",alpha)
+# 显示alpha通道
+cv2.imshow("alpha",alpha)
+cv2.waitKey(0)
+#将alpha通道数值的区间范围限制在0-1之间作为权
+alpha = alpha.astype(float)/255
+
+#读入背景图片
+background = cv2.imread(img_path_2+'bg.png')
+cv2.imshow("background",background)
+cv2.waitKey(0)
+#将前景图片去掉alpha通道
+foreground = cv2.merge((b,g,r))
+
+#将数值类型设置为浮点数防止溢出
+foreground = foreground.astype(float)
+background = background.astype(float)
+
+#对前景和背景利用alpha加权合成
+out = cv2.multiply(foreground,alpha)+cv2.multiply(background,1-alpha)
+# 保存合成的图像
+cv2.imwrite(img_path_2+"out.jpg",out)
+# 显示合成的图形
+cv2.imshow("out",out/255)
+cv2.waitKey(0)
+
+#关闭窗口
+cv2.destroyAllWindows()
