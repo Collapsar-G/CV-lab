a76ae085c7cc3a3f8d387c316cf8d97978cc867b
叶曦禾 <55824353+yexihe-jpg@users.noreply.github.com>
2020-11-07 10:03:44 +0800

    优化了lab1
    Signed-off-by: 叶曦禾 <55824353+yexihe-jpg@users.noreply.github.com>

--- lab1/lab1_1.py
+++ lab1/lab1_1.py
@@ -1,5 +1,7 @@
 import cv2.cv2 as cv2
+import matplotlib.pyplot as plt
 import pyglet
+from PIL import Image
 
 """"""
 """"""
@@ -8,13 +10,22 @@ import pyglet
 """"""
 
 img_path_1 = r'./image/lab1/'
-img_name_1 = ['Img1.png', 'Img2.jpg', 'Img3.bmp']
+img_name_1 = ['Img1.png', 'Img2.jpg', 'Img3.bmp', 'Img4.gif']
 img = []
-for i in range(0, 3):
-    image = cv2.imread(img_path_1 + img_name_1[i], 1)
+for i in range(0, 4):
+    # image = cv2.imread(img_path_1 + img_name_1[i], 1)
+    #
+    # im_file = open(img_path_1 + img_name_1[i])
+    # im_obj = imread(im_file)
+    image = Image.open(img_path_1 + img_name_1[i]).convert('RGB')
+    # cv2.imshow(img_name_1[i], img[i])
     img.insert(i, image)
-    cv2.imshow(img_name_1[i], img[i])
-
+    plt.subplot(2, 2, i + 1)
+    plt.imshow(img[i])
+    plt.title(img_name_1[i], fontsize=8)
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
 
 # 由于图片所在的目录不在当前目录，因此我们需要告诉Pyglet去哪里找到它们：
 pyglet.resource.path = ['./image/lab1/']
@@ -23,6 +34,7 @@ pyglet.resource.reindex()
 # 选择一个gif文件路径
 gif = pyglet.resource.animation('Img4.gif')
 sprite = pyglet.sprite.Sprite(gif)
+# for i in range(sprite.)
 
 # 创建窗口并将其大小调整为图像大小
 win = pyglet.window.Window(width=sprite.width, height=sprite.height, caption='Img4.gif')
